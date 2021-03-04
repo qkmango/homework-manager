463dfba1e3b431e175ad4ab6e0c87064a6965b3b
qkmango <2099432021@qq.com>
2021-03-04 11:55:25 +0800

    test

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,18 +1,34 @@
+import cn.qkmango.hm.exception.HomeworkException;
 import cn.qkmango.hm.system.domain.AliOss;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        delete();
 
-        // OSSServiceImpl.getOssSTS();
+        HashMap<String, Object> map = new HashMap<>();
 
+        Object put = map.put("name", "zs");
+
+        System.out.println(put);
     }
 
 
-    public static void delete() {
+    public static int delete() throws HomeworkException {
+
+        try {
+            throw new HomeworkException("失败");
+        } catch (HomeworkException e) {
+            e.printStackTrace();
+            throw e.getCause();
+        } finally {
+            return 1;
+        }
+
+
 
     }
 
