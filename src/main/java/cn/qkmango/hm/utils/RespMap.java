65663df8290a28086e1fb7134ad4a3d4a2181420
qkmango <2099432021@qq.com>
2021-03-04 11:52:16 +0800

    创建RespMap，专为response响应使用：内置putSuccess()、putMsg()，更加方便

---
+++ src/main/java/cn/qkmango/hm/utils/RespMap.java
@@ -0,0 +1,25 @@
+package cn.qkmango.hm.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @version 1.0
+ * @Description: //TODO
+ * <p>类简介</p>
+ * <p>类详细介绍</p>
+ * @className RespMap
+ * @author: Mango
+ * @date: 2021-03-04 10:52
+ */
+public class RespMap<V> extends HashMap<String,V> {
+
+    public V putSuccess(V flag) {
+        return super.put("success", flag);
+    }
+
+    public V putMsg(V msg) {
+        return super.put("msg", msg);
+    }
+
+}
